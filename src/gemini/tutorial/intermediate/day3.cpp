3408af3d4d57dbb56ec6d591aa3bea699d5b4c68
k-kobayashi <k.kounosuke126@gmail.com>
2026-02-23 15:22:46 +0900

    weak_ptr

---
+++ src/gemini/tutorial/intermediate/day3.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class ECU;
+
+class Car
+{
+    public:
+        shared_ptr<ECU> my_ecu;
+        Car();
+        ~Car();
+        void honk();
+};
+
+Car::Car(){
+    cout << "車を生成" << endl;
+}
+
+Car::~Car(){
+    cout << "車を破棄" << endl;
+}
+
+void Car::honk(){
+    cout << "プップー" << endl;
+}
+
+
+class ECU
+{
+    public:
+        weak_ptr<Car> my_car;
+        ECU();
+        ~ECU();
+        void checkCarStatus();
+};
+
+ECU::ECU(){
+    cout << "ECUを生成" << endl;
+}
+
+ECU::~ECU(){
+    cout << "ECUを破棄" << endl;
+}
+
+void ECU::checkCarStatus(){
+    shared_ptr<Car> temp_car = my_car.lock();
+    if (temp_car) {
+        temp_car->honk();
+    } else {
+        cout << "車はすでに破棄されています" << endl;
+    }
+}
+
+int main()
+{
+    shared_ptr<ECU> ecu = make_shared<ECU>();
+
+    {
+        shared_ptr<Car> car = make_shared<Car>();
+        car->my_ecu = ecu;
+        ecu->my_car = car;
+
+        cout << car.use_count() << ecu.use_count() <<endl;
+
+        ecu->checkCarStatus();
+        cout << car.use_count() << ecu.use_count() <<endl;
+    }
+    cout << ecu.use_count() <<endl;
+    ecu->checkCarStatus();
+    cout <<  ecu.use_count() <<endl;
+
+    return 0;
+}
